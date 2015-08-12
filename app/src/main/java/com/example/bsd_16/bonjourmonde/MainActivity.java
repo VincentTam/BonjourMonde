ef0eb8201bfc289d76d0f60c417bb1f70091e4c2
Vincent Tam <sere@live.hk>
2015-08-12 17:57:14 +0800

    Tried settings from a site
    http://jpress.tw/android-dev2#comment-1730
    
    misc.xml is automatically generated.
    activity_main.xml is editted first through the graphical preview, then editted manually.
    strings.xml is generated from clicking the light bulb in the IDE buffer for activity_main.xml.  This helps i18n.
    MainActivity.java contains a simpler method than VincentTam/MyFirstApp@f8f5474 suggested by the official startup guide of Android Developers.

--- app/src/main/java/com/example/bsd_16/bonjourmonde/MainActivity.java
+++ app/src/main/java/com/example/bsd_16/bonjourmonde/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView);
+    }
+
+    public void button1(View v) {
+        tv1.setText("hello");
     }
 
     @Override
@@ -18,20 +26,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
